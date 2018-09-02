77b49f0784f576454fac27474d5e073a84306ccd
suksanmasee <59160138@go.buu.ac.th>
2018-09-02 14:10:46 +0700

    Lab3_1 First Commit

---
+++ Project/src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+       // String input  = JOptionPane.showInputDialog("ใส่ตัวเลข 2 ตัว" ,"0");
+      //  JOptionPane.showMessageDialog(null,"message","title",JOptionPane.INFORMATION_MESSAGE);
+     int ans=  JOptionPane.showConfirmDialog(null,"รักหรือป่าว",
+                "ถามเธอ",JOptionPane.YES_NO_OPTION);
+
+    if (ans==JOptionPane.YES_OPTION){
+    JOptionPane.showMessageDialog(null,"รักจะ จุ๊ปๆ");
+    }  else {
+
+        JOptionPane.showMessageDialog(null,"จำไว้เลย");
+
+
+
+}
+    }
+
+
+}
